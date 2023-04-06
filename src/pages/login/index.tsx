c5aa7550f0488b2561c3959a6ecf68e140a37b55
jmbp1999 <pamudithaj98@gmail.com>
2023-04-06 20:22:16 +0530

    login page created with amplify integration

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @next/next/no-img-element */
+import { LockClosedIcon } from '@heroicons/react/20/solid'
+import { FcGoogle } from 'react-icons/fc';
+import { Auth } from 'aws-amplify';
+import { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth/lib/types";
+export default function Login() {
+    const handleGoogleLogin = ()=>{
+        Auth.federatedSignIn({provider: CognitoHostedUIIdentityProvider.Google });
+      }
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <h1 className="mt-6 text-center text-4xl font-bold tracking-tight text-gray-900">
+              Jotter
+            </h1>
+          </div>
+
+
+        <div style={{boxSizing:"border-box",width:"50",height:"100px"}} > 
+        <button
+              type="submit"
+                onClick={handleGoogleLogin}
+              className="
+              group relative flex items-center w-full justify-center rounded-sm 
+              bg-white border border-gray-300 px-3 py-2 text-sm font-medium 
+              text-gray-700 hover:bg-gray-50 
+              focus:ring-1  focus:ring-gray-800 focus:border-transparent"
+            >
+              <FcGoogle className="h-5 w-5 mr-2" aria-hidden="true" />
+              <span className='text-1xl font-bold'>Sign in with Google</span>
+            </button>
+        </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
