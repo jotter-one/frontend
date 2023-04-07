0ebece080ff33f9f4f81ae68586d355d79c472bd
jmbp1999 <pamudithaj98@gmail.com>
2023-04-07 13:01:07 +0530

    added layout to the whole app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,30 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import awsExports from "../aws-exports";
-import { Amplify } from 'aws-amplify';
+import awsExports from '../aws-exports'
+import { Amplify } from 'aws-amplify'
+import Navbar from '@/components/Navbar'
+import { NextPage } from 'next'
+import { ReactElement, ReactNode } from 'react'
 
+Amplify.configure({ ...awsExports, ssr: true })
 
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+   getLayout?: (page: ReactElement) => ReactNode
+}
 
-Amplify.configure({...awsExports,ssr:true});
+type AppPropsWithLayout = AppProps & {
+   Component: NextPageWithLayout
+}
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+   if (Component.getLayout) {
+      return Component.getLayout(<Component {...pageProps} />)
+   }
+   return (
+      <>
+         <Navbar />
+         <Component {...pageProps} />
+      </>
+   )
 }
