088850fb10269bc261155bb97496b738c4af83ba
jmbp1999 <pamudithaj98@gmail.com>
2023-04-08 22:34:47 +0530

    added emoji pallete updates

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -4,19 +4,28 @@ import styles from '../styles/Emoji.module.css'
 
 const EmojiPicker = () => {
    // State to keep track of the selected emoji
-   const [selectedEmoji, setSelectedEmoji] = useState<string | null>(null)
+   const [selectedEmoji, setSelectedEmoji] = useState<string[]>([])
 
    // Function to handle when an emoji is clicked
    const handleEmojiClick = (emoji: string) => {
-      console.log(emoji)
-      setSelectedEmoji(emoji)
+      setSelectedEmoji((prev) => {
+         return [...prev, emoji]
+      })
    }
 
    return (
       <div className={styles.emoji__picker}>
          {/* Display the selected emoji */}
-         <div className={styles.selected__emoji}>{selectedEmoji}</div>
-         {/* Display the list of emojis */}
+         <div className={styles.selected__emojis}>
+            {selectedEmoji.map((emoji, i) => {
+               return (
+                  <div key={i} className={styles.selected__emoji}>
+                     {emoji}
+                  </div>
+               )
+            })}
+         </div>
+
          <div className={styles.emoji__list}>
             {Object.keys(emojiData)
                .slice(0, 30)
