7ef9ab98fc68d14e3ae7bf01dca6fa74d4724b21
jmbp1999 <pamudithaj98@gmail.com>
2023-04-08 14:25:38 +0530

    created the text area

---
+++ src/components/Diary.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import styles from '../styles/Diary.module.css'
+function NotebookPaperTextarea() {
+   const [value, setValue] = useState('')
+
+   function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+      setValue(event.target.value)
+      event.target.style.height = 'auto'
+      event.target.style.height = event.target.scrollHeight + 'px'
+   }
+
+   return (
+      <div className='bg-white shadow rounded-md p-4'>
+         <div className='h-2 rounded-t-md'></div>
+         <div className='relative'>
+            <textarea
+               className={`bg-transparent border-none focus:outline-none w-full resize-none ${styles.text__area}`}
+               rows={5}
+               value={value}
+               onChange={handleChange}
+               spellCheck='false'
+            ></textarea>
+         </div>
+      </div>
+   )
+}
+
+export default NotebookPaperTextarea
