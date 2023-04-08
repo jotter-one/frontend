f80b52d2342ce0f1b0d4ff04f753eaadd96c57c3
jmbp1999 <pamudithaj98@gmail.com>
2023-04-08 15:15:20 +0530

    add two columns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,13 @@ import Diary from '../components/Diary'
 export default function Home() {
    return (
       <>
-         <Diary />
+         <div className='grid grid-cols-3 gap-4'>
+            <div className='col-span-2 '>
+               {' '}
+               <Diary />
+            </div>
+            <div className='col-span-1 '>Column 2</div>
+         </div>
       </>
    )
 }
