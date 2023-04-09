802f2be5417db393fb9a33dfef8b8731fed2b0d4
jmbp1999 <pamudithaj98@gmail.com>
2023-04-09 23:13:16 +0530

    added client side auth guard

---
+++ src/util/auth.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { Auth } from 'aws-amplify'
+
+type ProtectedRouteProps = {
+   children: React.ReactNode
+}
+
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+   const router = useRouter()
+   const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+   useEffect(() => {
+      async function checkAuth() {
+         try {
+            await Auth.currentAuthenticatedUser()
+            setIsAuthenticated(true)
+         } catch {
+            router.push('/login')
+         }
+      }
+
+      checkAuth()
+   }, [router])
+
+   if (!isAuthenticated) {
+      return null
+   }
+
+   return <div className='protected-component'>{children}</div>
+}
