a57891b7616636b6249d869fbc6e7ba0f3907897
jmbp1999 <pamudithaj98@gmail.com>
2023-04-09 23:52:01 +0530

    added configurations for aws

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,8 +6,10 @@ const awsExports = {
         userPoolWebClientId: "7i7h5rljnu29md0itos821grs0",
         oauth: {
             domain: "jotter-one.auth.us-east-1.amazoncognito.com",
-            redirectSignIn: "http://localhost:3000/",
-            redirectSignOut: "http://localhost:3000/login",
+            // redirectSignIn: "http://localhost:3000/",
+            // redirectSignOut: "http://localhost:3000/login",
+            redirectSignIn: "https://jotter-frontend.vercel.app/",
+            redirectSignOut: "https://jotter-frontend.vercel.app/login",
             scope: ['email', 'profile', 'openid'],
             responseType: 'token',
         },
