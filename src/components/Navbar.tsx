68c949fa91443d0b7ff727c5a0c85de32bb8fe66
jmbp1999 <pamudithaj98@gmail.com>
2023-04-14 16:47:54 +0530

    added logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,10 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 import { Auth } from 'aws-amplify'
 import Link from 'next/link'
+import Logo from '../../public/logo.png'
 const navigation = [
    { name: 'Home', href: '/' },
    { name: 'Wealth Manager', href: '/wealth' },
@@ -25,7 +27,7 @@ export default function Navbar() {
          {({ open }) => (
             <>
                <div className='mx-auto w-full px-2 sm:px-6 lg:px-8'>
-                  <div className='relative flex h-16 items-center justify-between'>
+                  <div className='relative flex h-12 items-center justify-between'>
                      <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
                         <Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
                            <span className='sr-only'>Open main menu</span>
@@ -38,17 +40,10 @@ export default function Navbar() {
                      </div>
                      <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
                         <div className='flex flex-shrink-0 items-center'>
-                           <img
-                              className='block h-8 w-auto lg:hidden'
-                              src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
-                              alt='Your Company'
-                           />
-                           <img
-                              className='hidden h-8 w-auto lg:block'
-                              src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
-                              alt='Your Company'
-                           />
+                           <Image className='block h-8 w-auto lg:hidden' src={Logo} alt='Your Company' />
+                           <Image className='hidden h-8 w-auto lg:block' src={Logo} alt='Your Company' />
                         </div>
+
                         <div className='hidden sm:ml-6 sm:block'>
                            <div className='flex space-x-4'>
                               {navigation.map((item) => (
@@ -56,10 +51,8 @@ export default function Navbar() {
                                     key={item.name}
                                     href={item.href}
                                     className={classNames(
-                                       router.route === item.href
-                                          ? 'bg-gray-900 text-white'
-                                          : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                       'rounded-md px-3 py-2 text-sm font-medium'
+                                       router.route === item.href ? 'text-white' : 'text-gray-300 hover:text-white',
+                                       'rounded-md px-2 py-1 text-sm font-medium'
                                     )}
                                     aria-current={router.route === item.href ? 'page' : undefined}
                                  >
