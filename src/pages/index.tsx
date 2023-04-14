2696b21cee80a9dfd2f5e322589e6f193c8882cd
jmbp1999 <pamudithaj98@gmail.com>
2023-04-14 17:50:23 +0530

    make responsive the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,11 @@ export default function Home() {
          <Head>
             <title>Home</title>
          </Head>
-         <div className='grid grid-cols-3 lg:grid-cols-3 md:grid-cols-6 gap-4'>
-            <div className='col-span-2 lg:col-span-2 md:col-span-3'>
+         <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-3'>
+            <div className='lg:col-span-2'>
                <Diary />
             </div>
-            <div className='col-span-1 lg:col-span-1 md:col-span-3 sm:w-full sm:flex-col'>
+            <div className='lg:col-span-1'>
                <div className='bg-white shadow rounded-md p-4'>
                   <EmojiPicker />
                </div>
