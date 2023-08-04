7a98b79e7853c5d059f9b4315a0f6d50302889dc
jmbp1999 <pamudithaj98@gmail.com>
2023-08-04 22:57:19 +0530

    changes project structure

--- src/pages/login/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-   return (
-      <div className='fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-opacity-50 bg-gray-800 z-50'>
-         <div className='animate-spin rounded-full border-t-4 border-blue-500 border-solid h-12 w-12'></div>
-      </div>
-   );
-}
