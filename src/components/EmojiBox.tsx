5c116374f11fa2aaeabafd6d1ce72ade79c7f35d
Hedes-Productions <tharindugimras@gmail.com>
2023-08-06 14:38:31 +0530

    Format the code

--- src/components/EmojiBox.tsx
+++ src/components/EmojiBox.tsx
@@ -2,19 +2,28 @@ import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import emojiBoxStyles from '../styles/EmojiBox.module.css';
 
 type emojiBoxParams = {
-    emoji: string,
-    text: string,
-    selectedEmoji: string,
-    setSelectedEmoji: Dispatch<SetStateAction<string>>
-}
+   emoji: string;
+   text: string;
+   selectedEmoji: string;
+   setSelectedEmoji: Dispatch<SetStateAction<string>>;
+};
 
-const EmojiBox = (params:emojiBoxParams) => {
-    const {emoji, text, selectedEmoji, setSelectedEmoji} = params;
+const EmojiBox = (params: emojiBoxParams) => {
+   const { emoji, text, selectedEmoji, setSelectedEmoji } = params;
 
-    return (<div className={`${emojiBoxStyles.emojiBox} ${selectedEmoji===emoji?emojiBoxStyles.emojiBoxSelected: emojiBoxStyles.emojiBoxNotSelected}`} onClick={()=>{setSelectedEmoji(emoji)}}>
-        <div className={emojiBoxStyles.emoji}>{emoji}</div>
-        <div className={emojiBoxStyles.text}>{text}</div>
-    </div>)
-}
+   return (
+      <div
+         className={`${emojiBoxStyles.emojiBox} ${
+            selectedEmoji === emoji ? emojiBoxStyles.emojiBoxSelected : emojiBoxStyles.emojiBoxNotSelected
+         }`}
+         onClick={() => {
+            setSelectedEmoji(emoji);
+         }}
+      >
+         <div className={emojiBoxStyles.emoji}>{emoji}</div>
+         <div className={emojiBoxStyles.text}>{text}</div>
+      </div>
+   );
+};
 
 export default EmojiBox;
