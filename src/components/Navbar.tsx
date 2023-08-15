6bbb3142947a63bf2cf1d86f1616da2710e9c39b
Hedes-Productions <tharindugimras@gmail.com>
2023-08-15 11:28:07 +0530

    Complete Avatar, Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,11 @@
 import Image from 'next/image';
 import navbarStyles from '../styles/Navbar.module.css';
 import logo from '../../public/logo.png';
-import profilePic from '../../public/profilepic.jpg';
 import Link from 'next/link';
 import HamburgerMenu from './HamburgerMenu';
 import { useState } from 'react';
+import Avatar from './Avatar';
+import { Auth } from 'aws-amplify';
 
 const navigation = [
    { name: 'Home', href: '/' },
@@ -15,6 +16,29 @@ const navigation = [
 
 const Navbar = () => {
    const [isClicked, setIsClicked] = useState(false);
+
+   const logOut = async () => {
+      try {
+         await Auth.signOut();
+         console.log('User logged out');
+      } catch (error) {
+         console.error('Error signing out:', error);
+      }
+   };
+
+   const avatarStyles = {
+      avatar: `${isClicked ? navbarStyles.avatarClicked : navbarStyles.avatarNotClicked}`,
+      avatarImg: `${isClicked ? navbarStyles.avatarImgClicked : navbarStyles.avatarImgNotClicked}`,
+   };
+
+   const optionsStyles = {
+      optionsBox: `${isClicked ? navbarStyles.optionsBoxClicked : navbarStyles.optionsBoxNotClicked}`,
+      option: `${isClicked ? navbarStyles.optionClicked : navbarStyles.optionNotClicked}`,
+   };
+
+   const customParams = {
+      hamburgerClicked: isClicked,
+   };
    return (
       <nav
          className={`${navbarStyles.navBar} ${isClicked ? navbarStyles.navBarClicked : navbarStyles.navBarNotClicked}`}
@@ -22,17 +46,43 @@ const Navbar = () => {
          <div className={navbarStyles.hamburgerMenu}>
             <HamburgerMenu clicked={isClicked} onClick={setIsClicked} />{' '}
          </div>
-         <Image src={logo} alt='logo' className={`${navbarStyles.logo} ${isClicked?navbarStyles.logoClicked:navbarStyles.logoNotClicked}`} />
-         <div className={`${navbarStyles.navLinks} ${isClicked?navbarStyles.navLinksClicked: navbarStyles.navLinksNotClicked}`}>
+         <Image
+            src={logo}
+            alt='logo'
+            className={`${navbarStyles.logo} ${isClicked ? navbarStyles.logoClicked : navbarStyles.logoNotClicked}`}
+         />
+         <div
+            className={`${navbarStyles.navLinks} ${
+               isClicked ? navbarStyles.navLinksClicked : navbarStyles.navLinksNotClicked
+            }`}
+         >
             {navigation.map((link) => {
                return (
-                  <Link key={link.name} href={link.href} className={`${navbarStyles.navLink} ${isClicked?navbarStyles.navLinkClicked:navbarStyles.navLinkNotClicked}`}>
+                  <Link
+                     key={link.name}
+                     href={link.href}
+                     className={`${navbarStyles.navLink} ${
+                        isClicked ? navbarStyles.navLinkClicked : navbarStyles.navLinkNotClicked
+                     }`}
+                  >
                      {link.name}
                   </Link>
                );
             })}
+            <div
+               onClick={() => logOut()}
+               className={`${navbarStyles.navLinkLogOut} ${
+                  isClicked ? navbarStyles.navLinkLogOutClicked : navbarStyles.navLinkLogOutNotClicked
+               }`}
+            >
+               Log Out
+            </div>
          </div>
-         <Image quality={100} src={profilePic} alt='profilepic' className={`${navbarStyles.profilePic} ${isClicked? navbarStyles.profilePicClicked: navbarStyles.profilePicNotClicked}`} />
+         <Avatar
+            customAvatarStyles={avatarStyles}
+            customAvatarOptionsStyles={optionsStyles}
+            customParams={customParams}
+         />
       </nav>
    );
 };
